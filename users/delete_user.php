d0248f5eb97c18d43e4fea35a9531c1ff5dd5d8e
yohannaignacia <yohannaignacia06@gmail.com>
2026-06-19 15:32:39 +0700

    Update delete_user.php

--- users/delete_user.php
+++ users/delete_user.php
@@ -1,7 +1,7 @@
 <?php
 header("Access-Control-Allow-Origin: *");
-header("Content-Type: application/json; charset=UTF-8");
-header("Access-Control-Allow-Methods: POST");
+header("Access-Control-Allow-Methods: GET, POST, OPTIONS");
+header("Access-Control-Allow-Headers: Content-Type");
 
 include_once '../config/database.php';
 
